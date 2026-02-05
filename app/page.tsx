a8478ccf8b8c0bd9ce2205220386c4b9042db157
Sheetal <sheetalbajajgodda@gmail.com>
2026-02-05 15:59:31 +0530

    updated folder

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomePage from "@/components/Home";
-
-export default function Home() {
-  return (
-     <div className="max-w-9/12 mx-auto mt-[70px]">
-         <HomePage />
-      </div>
-  );
-}
--- data/Appbar.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const navItems = [
-  { id: 1, label: "Signup", path: "/signup" },
-  { id: 2, label: "About", path: "/about" },
-  { id: 3, label: "Products", path: "/product" },
-  { id: 4, label: "Pricing", path: "/prices" },
-  { id: 5, label: "Support", path: "/support" }
-];
